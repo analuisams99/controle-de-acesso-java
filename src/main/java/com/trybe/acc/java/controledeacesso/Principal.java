2d14ef4d68f932adf00a3bf68f866743444cee3e
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-12-08 14:57:36 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -0,0 +1,11 @@
+package com.trybe.acc.java.controledeacesso;
+
+public class Principal {
+
+  /**
+   * Método principal.
+   */
+  public static void main(String[] args) {
+    // ESCREVA SEU CÓDIGO AQUI
+  }
+}
